6932691b0fa8be145d4760a66f348fec95e65fda
fengchj <hzfengchj@corp.netease.com>
2013-07-22 00:13:31 +0800

    add en0 sniff.

--- shutdown.sh
+++ shutdown.sh
@@ -4,6 +4,7 @@
 ps aux | grep raw-data-parse.sh | grep -v grep | awk '{print $2}' | xargs kill -9
 ps aux | grep "python ./lastfm-scrobler.py"  | grep -v grep | awk '{print $2}' | xargs kill -9
 ps aux | grep "ngrep -l -M -q -d en1 -Wbyline method=Playlog.add"  | grep -v grep | awk '{print $2}' | xargs kill -9
+ps aux | grep "ngrep -l -M -q -d en0 -Wbyline method=Playlog.add"  | grep -v grep | awk '{print $2}' | xargs kill -9
 ps aux | grep "tail -n 200 -f history"  | grep -v grep | awk '{print $2}' | xargs kill -9
 
 
--- sniff-packet.sh
+++ sniff-packet.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 ngrep -l -M -q -d en1 -Wbyline method=Playlog.add 
+ngrep -l -M -q -d en0 -Wbyline method=Playlog.add 
